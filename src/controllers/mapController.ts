f9849aaf41a6636c16a3c3af76732fc417efc656
felixelement <felix@element6.io>
2024-01-15 08:19:34 -0500

    done be

--- src/controllers/mapController.ts
+++ src/controllers/mapController.ts
@@ -15,8 +15,37 @@ export const getMapData = async (req: Request, res: Response) => {
 
     // Send the data as JSON
 
-    return responseServer(res, statusConstants.SUCCESS_CODE, result.rows);
+    return responseServer(
+      res,
+      statusConstants.SUCCESS_CODE,
+      "Connected Successfully",
+      result.rows
+    );
     // res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching data from the database:", error);
+    return raiseException(
+      res,
+      statusConstants.SERVER_ERROR_CODE,
+      "Internal Server Error"
+    );
+  }
+};
+
+export const getCircle = async (req: Request, res: Response) => {
+  const { lng, lat, radius } = req.query;
+
+  try {
+    const result = await pool.query(
+      `SELECT ST_AsGeoJSON(ST_Buffer(ST_GeomFromText('POINT(${lng} ${lat})'), ${radius})) as circle`
+    );
+
+    return responseServer(
+      res,
+      statusConstants.SUCCESS_CODE,
+      "Connected Successfully",
+      result.rows
+    );
   } catch (error) {
     console.error("Error fetching data from the database:", error);
     return raiseException(
@@ -31,14 +60,24 @@ export const getAreaCentroidInside = async (req: Request, res: Response) => {
   const { lng, lat, radius } = req.query;
 
   try {
-    // Fetch data from dfw_demo table
     const result = await pool.query(
-      `SELECT * from (select *,ST_AsText(ST_centroid(spatialobj)) as centroid from ${process.env.DB_TABLE_DEMO}) as tmp where ST_Within(centroid, ST_Buffer(ST_GeomFromText('POINT(${lng} ${lat})'), ${radius}))`
+      `SELECT income, population, ST_AsGeoJSON(spatialobj) as geometry from (
+        select *,ST_AsText(ST_centroid(spatialobj)) as centroid, 
+        ST_Buffer(ST_GeomFromText('POINT(${lng} ${lat})'), ${radius}) as circle
+        from ${process.env.DB_TABLE_DEMO}
+      ) as tmp 
+      where ST_Within(
+        centroid, 
+        circle
+      )`
     );
 
-    // Send the data as JSON
-    // res.json(result.rows);
-    return responseServer(res, statusConstants.SUCCESS_CODE, result.rows);
+    return responseServer(
+      res,
+      statusConstants.SUCCESS_CODE,
+      "Connected Successfully",
+      result.rows
+    );
   } catch (error) {
     console.error("Error fetching data from the database:", error);
     return raiseException(
@@ -46,7 +85,6 @@ export const getAreaCentroidInside = async (req: Request, res: Response) => {
       statusConstants.SERVER_ERROR_CODE,
       "Internal Server Error"
     );
-    // res.status(statusConstants.SERVER_ERROR_CODE).send("Internal Server Error");
   }
 };
 
@@ -56,19 +94,21 @@ export const getAreaInsideCircle = async (req: Request, res: Response) => {
   try {
     // Fetch data from dfw_demo table
     const result = await pool.query(
-      `SELECT * FROM dfw_demo 
+      `SELECT AVG(income) as avg_income FROM dfw_demo
       WHERE ST_Intersects(
-        spatialobj, 
+        spatialobj,
         ST_Buffer(ST_GeomFromText('POINT(${lng} ${lat})'), ${radius})::geography
       );`
     );
 
-    // Send the data as JSON
-    // res.json(result.rows);
-    return responseServer(res, statusConstants.SUCCESS_CODE, result.rows);
+    return responseServer(
+      res,
+      statusConstants.SUCCESS_CODE,
+      "Connected Successfully",
+      result.rows
+    );
   } catch (error) {
     console.error("Error fetching data from the database:", error);
-    // res.status(statusConstants.SERVER_ERROR_CODE).send("Internal Server Error");
     return raiseException(
       res,
       statusConstants.SERVER_ERROR_CODE,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 import express, { Router } from "express";
 dotenv.config();
 
-import { getAreaCentroidInside, getAreaInsideCircle, getMapData } from "@controllers/mapController";
+import { getAreaCentroidInside, getAreaInsideCircle, getCircle, getMapData } from "@controllers/mapController";
 
 const router: Router = express.Router();
 
 router.get("/map", getMapData);
+router.get("/circle", getCircle);
 router.get("/area-centroid-inside", getAreaCentroidInside);
 router.get("/area-inside-circle", getAreaInsideCircle);
 
